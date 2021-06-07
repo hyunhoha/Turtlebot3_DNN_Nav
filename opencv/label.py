a065208abd4affebf7d9653d10230a29a091e2fa
hyunhoha <65225823+hyunhoha@users.noreply.github.com>
2021-06-08 01:16:20 +0900

    Re-update(middle)
    middle-final
    추가 개선사항 존재

--- opencv/label.py
+++ opencv/label.py
@@ -6,37 +6,14 @@ import matplotlib.pyplot as plt
 with open("/home/iasl/catkin_ws/src/opencv/label.txt","a") as f:
     for i in range(1,501):
         img_name = "/home/iasl/catkin_ws/src/opencv/image/camera_image" + str(i) + ".jpeg"
-        # print(img_name)
         curr_img = cv2.imread(img_name,cv2.IMREAD_UNCHANGED)
-        # curr_img2 = cv2.resize(curr_img, dsize=(640,480), interpolation=cv2.INTER_AREA)
-        print(curr_img.shape)
-        # cv2.imshow(str(i), curr_img2)
-        # plt.ion()
-        # plt.
         name2 = str(i)
         cv2.namedWindow(name2, cv2.WINDOW_NORMAL)
         cv2.imshow(name2,curr_img)
-        # plt.imshow(curr_img)
-        # time.sleep(3)
-
-        # while True:
             
         key = cv2.waitKey(100)
-        # print(str(key))
-        # if key == 27:
-        #     print("good")
-        #     pass
-        # f.write(chr(key)+" ")
-        # pass
-        # if key == 48 or key==49 or key == 50: # 0, 1, 2
-        #     print(chr(key))
-        #     f.write(chr(key)+" ")
-        #     pass
 
-        # plt.imshow(curr_img)
         curr_label = str(input())
-        # print(curr_label)
-        # curr_label = key
         f.write(curr_label+" ")
         cv2.destroyAllWindows()
         time.sleep(0.1)
--- opencv/src/image.py
+++ opencv/src/image.py
@@ -1,18 +1,9 @@
 #! /usr/bin/python3
-# Copyright (c) 2015, Rethink Robotics, Inc.
 
-# Using this CvBridge Tutorial for converting
-# ROS images to OpenCV2 images
-# http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-
-# Using this OpenCV2 tutorial for saving Images:
-# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
-
-# rospy for the subscriber
 import rospy
 # ROS Image message
 from sensor_msgs.msg import Image
-# ROS Image message -> OpenCV2 image converter
+
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
@@ -21,10 +12,6 @@ import numpy as np
 # Instantiate CvBridge
 bridge = CvBridge()
 global current_img 
-# global count
-# global label
-# global count
-# global img_count
 
 def image_callback(msg):
     print("Received an image!")
@@ -48,26 +35,9 @@ def image_callback(msg):
             img_count=0
         
         img_count += 1
-        # else:
-        #     return None
+
     except:
         print("Except")
-    # except CvBridgeError, e:
-    #     print(e)
-    # else:
-    #     # Save your OpenCV2 image as a jpeg 
-    #     # cv2.imwrite('camera_image.jpeg', cv2_img)
-    #     # cv2.imwrite('camera_image.jpeg', cv_image_norm)
-    #     cv2.imwrite('camera_image'+str(count)+'.jpeg', current_img)
-    #     count += 1
-
-# def store_img_and_label(num):
-#     global count
-#     cv2.imwrite('camera_image' + str(count)+'.jpeg', current_img)
-#     f = open('label.txt', 'a')
-#     f.write(str(num))
-#     f.close()
-#     count += 1
 
 def main():
     global img_count
@@ -76,16 +46,8 @@ def main():
     rospy.init_node('image_listener')
     # Define your image topic
     image_topic = "/camera/depth/image_raw"
-    # label_topic = "/label"
-    # Set up your subscriber and define its callback
-    
+
     rospy.Subscriber(image_topic, Image, image_callback)
-    # img_count += 1
-    # count += 1
-    # label_num = input("Type label(0~9) : ")
-    # store_img_and_label(label_num)
-    # rospy.Subscriber(label_topic, label, label_callback)
-    # Spin until ctrl + c
     rospy.spin()
 
 if __name__ == '__main__':
