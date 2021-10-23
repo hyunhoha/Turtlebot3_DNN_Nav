5e3699b53110c6f05810d45e141e145929531e2e
hyunhoha <jghsa6665@naver.com>
2021-10-24 03:50:39 +0900

    cleaned some dirty files

--- 210716_coll_4classes/knu_prj/src/turtlesim_depth_control_classed_coll_4_classed_0.py
+++ 210716_coll_4classes/knu_prj/src/turtlesim_depth_control_classed_coll_4_classed_0.py
@@ -6,8 +6,6 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Int16
 # ROS Image message -> OpenCV2 image converter
 from cv_bridge import CvBridge, CvBridgeError
-# OpenCV2 for saving an image
-# from gazebo_msgs import ModelStates
 
 import cv2
 import numpy as np
@@ -21,17 +19,15 @@ config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
-
 bridge = CvBridge()
-# model = tf.keras.models.load_model("/home/iasl/catkin_ws/src/knu_prj/src/DNN_model/")
-# model = tf.keras.models.load_model("/home/iasl/catkin_ws/src/knu_prj/res_class_model2/")
 model = tf.keras.models.load_model("/home/iasl/catkin_ws/src/knu_prj/res_collision_model_new_4_classes_0_labeled/")
 control_out = 0
-# pub = rospy.Publisher('ctr_out', Int16 , queue_size =10)
+
 pub = rospy.Publisher('cmd_vel', Twist , queue_size =10)
 
 def image_callback(image_data):
-    # global control_out
+    # get image to predict control
+
     global twist_data
     depth_image = bridge.imgmsg_to_cv2(image_data, "32FC1")
     cv_image_array = np.array(depth_image, dtype = np.dtype('f8'))
@@ -40,22 +36,22 @@ def image_callback(image_data):
     img_norm = np.uint8(img_norm)
     image = np.array(img_norm).reshape((1,224,224,1))
     pred_out = model.predict(image)
-    # print("pred out : " , pred_out)
+
+    # process image and predict using deep-learning model.
+
     control_out = pred_out[0][0]
     collision_prob = pred_out[1][0]
+
     print("control : ", control_out)
     print("collision prob : ", collision_prob)
 
+    # parameters
     a, b, c, d = -0.05, 3, 0.2, 3
+    
     alpha = 0.9
-    # new_vel = (control_out[0] * a) + (control_out[2] * c) - collision_prob[0]*0.15
     new_vel = (1 - control_out[0] + control_out[2]) * c - collision_prob[0]*0.1
     twist_data.linear.x = alpha * new_vel + (1-alpha) * twist_data.linear.x 
-    # if collision_prob[0] >= 0.1:
-        
-    # else:
-    #     twist_data.linear.x = alpha * new_vel + (1-alpha) * twist_data.linear.x
-
+   
     if control_out[1] >= control_out[3]:
         twist_data.angular.z = (0.01+ control_out[0] + collision_prob[0]) * alpha * 0.5 + (1-alpha) * twist_data.angular.z
     else:
@@ -64,39 +60,9 @@ def image_callback(image_data):
     pub.publish(twist_data)
     print("control : ", twist_data.linear.x, twist_data.angular.z)
 
-# def status_callback(status_data):
-#     global crush_state
-
-#     if status_data.twist.linear.x <= 0.01:
-#         crush_state = True
-
-# def image_callback(image_data):
-#     global twist_data
-#     # global crush_state
-
-#     # if crush_state:
-#     #     twist_data.linear.x = -0.1
-#     #     twist_data.angular.z = 0
-#     #     pub.publish(twist_data)
-#     # else:
-#     depth_image = bridge.imgmsg_to_cv2(image_data, "32FC1")
-#     cv_image_array = np.array(depth_image, dtype = np.dtype('f8'))
-#     img_re = cv2.resize(cv_image_array, dsize=(224,224), interpolation=cv2.INTER_LINEAR)
-#     img_norm = cv2.normalize(img_re, img_re, 0, 255, cv2.NORM_MINMAX)
-#     img_norm = np.uint8(img_norm)
-#     image = np.array(img_norm).reshape((1,224,224,1))
-#     # control_out = np.argmax(model.predict(image))
-#     print("predicted : ", model.predict(image))
-#     twist_data.linear.x, twist_data.angular.z = model.predict(image)[0]
-#     # pub.publish(control_out)
-#     pub.publish(twist_data)
-#     # print("control : ", control_out)
-
 def main():
-    # global twist_data
+
     image_topic = "/camera/depth/image_raw"
-    # status_topic = "/gazebo/model_states/"
-    # rospy.Subscriber(status_topic, ModelStates, status_callback)
     rospy.Subscriber(image_topic, Image, image_callback)
     rospy.spin()
 
