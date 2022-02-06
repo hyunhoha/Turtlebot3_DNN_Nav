921e2474a4c24b80e3c2ead09aa710310525e264
hyunhoha <jghsa6665@naver.com>
2022-02-07 04:19:09 +0900

    updated

--- src/save_image_and_label.py
+++ src/save_image_and_label.py
@@ -3,34 +3,16 @@
 # rospy for the subscriber
 import rospy
 from sensor_msgs.msg import Image
-#include "geometry_msgs/Twist.h"
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 from nav_msgs.msg import Odometry
 
-# import math
-
 # Instantiate CvBridge
 bridge = CvBridge()
-# global current_img 
 
-# current_vel = Twist()
-
-# def odom_callback(odom):
-#     global current_vel
-#     linx = odom.twist.twist.linear.x
-#     angz = odom.twist.twist.angular.z
-#     # print("Received : ", linx, angz)
-#     if linx >= 0.25:
-#         current_vel.linear.x = 0.25
-#     else:
-#         current_vel.linear.x = round(linx, 2)
-    
-#     current_vel.angular.z = round(angz, 2)
-
-
+# Velocity가 들어올 때 마다 global 변수인 현재 속도를 갱신한다.
 def velocity_callback(msg):
     global current_vel
     current_vel = msg
@@ -40,20 +22,8 @@ def velocity_callback(msg):
         current_vel.linear.x = round(msg.linear.x, 2)
 
     current_vel.angular.z = round(msg.angular.z, 2)
-
-    # print("received an image")
-    # global img_count
-    # global count
-    # s2 = '/home/iasl/catkin_ws/src/knu_prj/label.txt'
-
-    # try:
-    #     if img_count % 20 == 0:
-    #         f = open(s2, 'a')
-    #         f.write(str(msg.linear.x) + " " + str(msg.angular.z) + "\n")
-    
-    # except:
-    #     print("Except_vel")
             
+# 이미지가 들어올 때 현재 속도랑 이미지를 동시에 저장한다.
 def image_callback(msg):
     # print("Received an image!")
     # print(count)
@@ -61,34 +31,35 @@ def image_callback(msg):
     global count
     global current_vel
     # print(img_count)
-    # print(count)
 
     try:
-        # if img_count % 2 == 0:
-        # Hz 1/20 (Slower)
-        # if current_vel.linear.x < 0 or (current_vel.linear.x == 0 and current_vel.angular.z == 0):
-        #     return None
-        # else:
-        cv2_img = bridge.imgmsg_to_cv2(msg, "32FC1")#"bgr8")
-        cv_image_array = np.array(cv2_img, dtype = np.dtype('f8'))
-        cv_image_norm = cv2.normalize(cv_image_array, cv_image_array, 0, 255, cv2.NORM_MINMAX)
-        
-        # current_img = cv_image_norm
-        s='/home/hyun/catkin_ws/src/knu_prj/test_imgsss/camera_image'+str(count)+'.jpeg'
-        # s='/home/iasl/catkin_ws/src/knu_prj/new_images_res/camera_image'+str(count)+'.jpeg'
+        # 이미지 들어오는 속도가 너무 빠르면 조절할 수 있음.
+        # img_count라는걸 새로 만들어서, 속도를 n배 늦출 수 있음.
+        # count = 이미지 저장용 숫자.
 
-        s2 = '/home/hyun/catkin_ws/src/knu_prj/new_label_res.txt'
-        
-        # print s
-        cv2.imwrite(s, cv_image_norm)
-        f = open(s2, 'a')
-        f.write(str(current_vel.linear.x) + " " + str(current_vel.angular.z) + "\n")
-        count += 1
-        img_count=0
+        if img_count % 5 == 0:   # 늦추려면 5를 더 크게 변경
+
+            if current_vel.linear.x < 0 or (current_vel.linear.x == 0 and current_vel.angular.z == 0):
+                return None
+
+            cv2_img = bridge.imgmsg_to_cv2(msg, "32FC1")#"bgr8")
+            cv_image_array = np.array(cv2_img, dtype = np.dtype('f8'))
+            cv_image_norm = cv2.normalize(cv_image_array, cv_image_array, 0, 255, cv2.NORM_MINMAX)
+
+            # 이미지 저장 폴더 경로, txt파일 경로             
+            s='/home/hyun/catkin_ws/src/knu_prj/ㅁㅁ/camera_image'+str(count)+'.jpeg'
+            s2 = '/home/hyun/catkin_ws/src/knu_prj/ㅁㅁ.txt'
             
-        # img_count += 1
-        # else:
-        #     return None
+            cv2.imwrite(s, cv_image_norm)
+            f = open(s2, 'a')
+            f.write(str(current_vel.linear.x) + " " + str(current_vel.angular.z) + "\n")
+
+            count += 1
+            img_count=1
+            
+        else:
+            img_count += 1
+
     except:
         print("Except")
 
@@ -98,13 +69,10 @@ def main():
     
 
     rospy.init_node('image_listener_save')
-    # Define your image topic
     image_topic = "/camera/depth/image_raw"
     velocity_topic = "/cmd_vel"
-    # odom_topic = "/odom"
     
     rospy.Subscriber(velocity_topic, Twist, velocity_callback)
-    # rospy.Subscriber(odom_topic, Odometry, odom_callback)
     rospy.Subscriber(image_topic, Image, image_callback)
     rospy.spin()
 
--- src/turtle_depth_goal_real_remastered.py
+++ src/turtle_depth_goal_real_remastered.py
@@ -1,12 +1,10 @@
 #! /usr/bin/python3
+
 from os import stat
 
-# from matplotlib.colors import TwoSlopeNorm
 import rospy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
-# from std_msgs.msg import Int16
-# ROS Image message -> OpenCV2 image converter
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
@@ -24,8 +22,6 @@ import math
 import sys
 import time
 
-# import argparse
-
 from std_msgs.msg import Float32
 
 config = ConfigProto()
@@ -33,199 +29,32 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 bridge = CvBridge()
-# model = tf.keras.models.load_model("/home/hyun/catkin_ws/src/knu_prj/res_collision_model_new_4_classes_0_labeled/")
-model = tf.keras.models.load_model("/home/hyun/catkin_ws/src/knu_prj/remastered_model_220121/")
-# model = tf.keras.models.load_model("/home/hyun/catkin_ws/src/knu_prj/remastered_model_22012(4)_good/")
 
+# 모델 load
+model = tf.keras.models.load_model("/home/hyun/catkin_ws/src/knu_prj/remastered_model_220121/")
 
 control_out = 0
+
 pub = rospy.Publisher('cmd_vel', Twist , queue_size =1)
 pub2 = rospy.Publisher('collision', Float32, queue_size= 1)
 
+# 목표 위치
 goal = [2.0, 1.5]
-# goal = [2.0, -2.0]
 
-
-tmp_goal = [0, 0]
-my_col_prob = 0
-local_flag = False
-cnt = 0
+# 시작 위치
 start = [0, 0]
 
 
 start_time = time.time()
 has_reached = False
 
-# def parse_args(argv=None):
-#     parser = argparse.ArgumentParser(
-#         description='YOLACT COCO Evaluation')
-#     parser.add_argument('--x',
-#                         default=2.0, type=float,
-#                         help='goal position x')
-#     parser.add_argument('--y', default=1.5, type=float,
-#                         help='goal position y')
-    # parser.set_defaults(no_bar=False, display=False, resume=False, output_coco_json=False, output_web_json=False, shuffle=False,
-    #                     benchmark=False, no_sort=False, no_hash=False, mask_proto_debug=False, crop=True, detect=False, display_fps=False,
-    #                     emulate_playback=False)
-
-
-
-def image_callback(image_data):
-    global twist_data
-    global my_col_prob
-    global local_flag
-    global tmp_goal
-    global cnt
-    global has_reached
-
-    if has_reached:
-        return
-
-    depth_image = bridge.imgmsg_to_cv2(image_data, "32FC1")
-    cv_image_array = np.array(depth_image, dtype = np.dtype('f8'))
-    img_re = cv2.resize(cv_image_array, dsize=(224,224), interpolation=cv2.INTER_LINEAR)
-    img_norm = cv2.normalize(img_re, img_re, 0, 255, cv2.NORM_MINMAX)
-    img_norm = np.uint8(img_norm)
-    image = np.array(img_norm).reshape((1,224,224,1))
-    pred_out = model.predict(image)
-    # print("pred out : " , pred_out)
-    control_out = pred_out[0][0]
-    collision_prob = pred_out[1][0]
-
-    # print("prediction : ", control_out)
-    # print("collision prob : ", collision_prob)
-    
-    dx = goal[0] - state[0]
-    dy = goal[1] - state[1]
-
-    a, b, c, d = -0.05, 3, 0.2, 3
-    alpha = 0.9
-    beta = 0.7
-    # new_vel = (control_out[0] * a) + (control_out[2] * c) - collision_prob[0]*0.15
-
-    # my_col_prob = collision_prob * 0.7 + my_col_prob * 0.3
-
-    if abs(dy) < 0.1 and abs(dx) < 0.1:
-        twist_data.linear.x = 0
-        twist_data.angular.z = 0
-        print("reached")
-        pub.publish(twist_data)
-        print("--- %s seconds has been take to move from (%f, %f) to (%f, %f)" % (time.time() - start_time, start[0], start[1], goal[0], goal[1]))
-        # sys.exit(0)
-        has_reached = True
-        return
-    # elif abs(dx) <0.1 and dy>=0:
-    #     new_ang = 3.1415/2 - state[2]
-    # elif abs(dx) <0.1 and dy<0:
-    #     new_ang = -3.1415/2 - state[2]
-    # elif abs(dy) <0.1 and dx>=0:
-    #     new_ang = -state[2]
-    # elif abs(dy) <0.1 and dx<0:
-    #     new_ang = 3.1415 - state[2]
-    else:
-        tmp = math.atan2(dy, dx)#) - state[2]
-        if tmp <= 0.05:
-            tmp += 6.2832
-        new_ang = tmp - state[2]
-        if new_ang < -3.1415:
-            new_ang += 6.2832
-        elif new_ang > 3.1415:
-            new_ang -= 6.2832
-    
-    # print("goal angle : ", tmp*180/3.1415, " current : ", state[2]*180/3.1415, "Vel : ", new_ang)
-
-    # if collision_prob <= 0.1:
-    #     twist_data.linear.x = beta * 0.26 + (1-beta) * twist_data.linear.x    
-    #     # print("atan : ", new_ang, "odom theta : ", state[2])
-    #     # twist_data.angular.z = (collision_prob[0]+0.05) * 3 * new_ang * beta + (1-collision_prob[0]) * 0.5 * (1-beta) * twist_data.angular.z
-    #     twist_data.angular.z = 0
-
-    # control_out[1] *0.264
-
-    # if twist_data.angular.z >= 0 and collision_prob > 0.5:
-    #     control_out[0] *= (1+twist_data.angular.z)
-    # elif twist_data.angular.z <0 and collision_prob > 0.5:
-    #     control_out[2] *= (1-twist_data.angular.z)
-
-    # print("prediction2 : ", control_out)
-
-
-    if collision_prob <= 0.1:
-    # if my_col_prob <= 0.5:
-        twist_data.linear.x = beta * 0.26 + (1-beta) * twist_data.linear.x    
-        # print("atan : ", new_ang, "odom theta : ", state[2])
-        # twist_data.angular.z =  0.5 * new_ang * beta + (1-beta) * twist_data.angular.z
-
-        twist_data.angular.z =  0.4 * new_ang * beta + (1-beta) * twist_data.angular.z
-
-        # twist_data.angular.z =  (0.4-0.1 * collision_prob[0]) * new_ang * beta + (1-beta) * twist_data.angular.z
-
-
-
-        # twist_data.angular.z = 0.5 * new_ang * beta + (1-beta) * twist_data.angular.z
-
-        local_flag = False
-        
-        # beta -= 0.05
-    else:
-        new_vel = (1 + control_out[1]) * c - collision_prob[0]*0.4
-        twist_data.linear.x = beta * new_vel + (1-beta) * twist_data.linear.x
-
-        if control_out[0] < 0.1 and control_out[2] < 0.1:
-            twist_data.angular.z = 0
-        
-        elif control_out[0] >= control_out[2]:# and new_ang >= 0:
-            # twist_data.angular.z = (0.01+ collision_prob[0]) * beta + (1-beta) * twist_data.angular.z
-
-            twist_data.angular.z = (0.1+ collision_prob[0] + control_out[0]) * beta * 0.7 + (1-beta) * twist_data.angular.z
-
-            
-            # twist_data.angular.z = (1+ collision_prob[0]) * control_out[0]
-        elif control_out[0] < control_out[2]:# and new_ang < 0:
-            twist_data.angular.z = -(0.01 + collision_prob[0] + control_out[2]) * beta * 0.7 + (1-beta) * twist_data.angular.z
-            
-            # twist_data.angular.z = -(1 + collision_prob[0]) * control_out[2]
-                    # beta += 0.05
-
-    if twist_data.linear.x < -0.1:
-        twist_data.linear.x = -0.05
-    # twist_data.angular.z = -twist_data.angular.z
-    # print(collision_prob[0])
-
-        # elif control_out[1] >= control_out[3]:
-        #     tmp_goal = [state[0] ]
-        # else:
-        #     local_flag = True
-        #     twist_data.linear.x = (1-beta) * twist_data.linear.x
-            
-        #     if cnt == 20:
-        #         # tmp_goal = [state[0] - goal[0], state[1] - goal[1]]
-        #         tmp_goal = [state[0], goal[1]]
-        #     elif cnt == 40:
-        #         tmp_goal = [goal[0], state[1]]
-        #         cnt = 0
-        #     cnt += 1
-
-    # control_out[0] = round(control_out[0], 2)
-    # control_out[1] = round(control_out[1], 2)
-    # control_out[2] = round(control_out[2], 2)
-
-    # print("prediction : ", control_out)
-    # print("collision prob : ", collision_prob)
-
-
-    cols = Float32()
-    cols.data = collision_prob[0]
-    pub.publish(twist_data)
-    pub2.publish(cols)
-    # print("cur x : ", state[0], " ", "cur y : ", state[1], "cur orientation : ", 180/3.1415*state[2], "ang_diff : ", 180/3.1415*math.atan2(goal[0]-state[0],goal[1]-state[1]))
-    # print("control : ", twist_data.linear.x, twist_data.angular.z)
-    
+# 로봇의 현재 상태를 갱신한다 (로봇의 위치, 보고 있는 방향)
 def odom_callback(odom_data):
     global state
     global start
     ori = odom_data.pose.pose.orientation
 
+    # state = [x, y, 현재 보고있는 방향(direction)] 의 크기 3짜리 배열임.
     state[0] = odom_data.pose.pose.position.x
     state[1] = odom_data.pose.pose.position.y
     tmp = tft.transformations.euler_from_quaternion([ori.x, ori.y, ori.z, ori.w])[2]
@@ -237,23 +66,110 @@ def odom_callback(odom_data):
     if start[0] == 0 and start[1] == 0:
         start[0] = state[0]
         start[1] = state[1]
-    # print("Current orientation : ", state[2]*180/3.1415)
-    # print("current orientation : ", state[2]*180/3.1415)
+
+
+def image_callback(image_data):
+    global twist_data
+    global has_reached
+
+    # 도착 했으면 움직이지 말고 멈춘다. 
+    if has_reached:
+        return
+
+    # 들어온 이미지를 모델에 넣어서 분류 확률이랑 충돌 확률을 얻는다.
+    depth_image = bridge.imgmsg_to_cv2(image_data, "32FC1")
+    cv_image_array = np.array(depth_image, dtype = np.dtype('f8'))
+    img_re = cv2.resize(cv_image_array, dsize=(224,224), interpolation=cv2.INTER_LINEAR)
+    img_norm = cv2.normalize(img_re, img_re, 0, 255, cv2.NORM_MINMAX)
+    img_norm = np.uint8(img_norm)
+    image = np.array(img_norm).reshape((1,224,224,1))
+    pred_out = model.predict(image)
+
+
+    # 분류된 [좌회전, 직진, 우회전 확률]과, [충돌확률(0~1사이)]
+    control_out = pred_out[0][0]
+    collision_prob = pred_out[1][0]
+
+    # print("prediction : ", control_out)
+    # print("collision prob : ", collision_prob)
+    
+    # 목표 위치랑 지금 로봇의 위치 차이
+    dx = goal[0] - state[0]
+    dy = goal[1] - state[1]
+
+    # 하이퍼파라미터
+    a, b, c, d = -0.05, 3, 0.2, 3
+    alpha = 0.9
+    beta = 0.7
+
+    # 목표 지점에 도달한 경우.
+    if abs(dy) < 0.1 and abs(dx) < 0.1:
+        twist_data.linear.x = 0
+        twist_data.angular.z = 0
+        print("reached")
+        pub.publish(twist_data)
+        print("--- %s seconds has been take to move from (%f, %f) to (%f, %f)" % (time.time() - start_time, start[0], start[1], goal[0], goal[1]))
+        has_reached = True
+        return
+
+    else:
+        # 로봇이 가야할 방향 (회전해야할 방향 계산)
+        tmp = math.atan2(dy, dx)#) - state[2]
+        if tmp <= 0.05:
+            tmp += 6.2832
+        new_ang = tmp - state[2]
+        if new_ang < -3.1415:
+            new_ang += 6.2832
+        elif new_ang > 3.1415:
+            new_ang -= 6.2832
+    
+    # 충돌 확률이 ㅁ% 밑이면, 로봇을 목표점 방향으로 회전하도록 한다. (전역 경로 추종) 
+    if collision_prob <= 0.1:
+        twist_data.linear.x = beta * 0.26 + (1-beta) * twist_data.linear.x    
+        twist_data.angular.z =  0.4 * new_ang * beta + (1-beta) * twist_data.angular.z
+
+    # 충돌 확률이 높으면, 깊이 이미지 기반 회피 주행을 한다. (지역 경로 추종)    
+    else:
+        # 직진 속도 계산
+        new_vel = (1 + control_out[1]) * c - collision_prob[0]*0.4
+        twist_data.linear.x = beta * new_vel + (1-beta) * twist_data.linear.x
+
+        # 좌우회전 확률 낮으면 그냥 직진
+        if control_out[0] < 0.1 and control_out[2] < 0.1:
+            twist_data.angular.z = 0
+        
+        # 우회전이랑 좌회전 확률 중 높은것만 채택
+        # 좌회전인경우
+        elif control_out[0] >= control_out[2]:
+            # Angular velocity는 양수
+            twist_data.angular.z = (0.1+ collision_prob[0] + control_out[0]) * beta * 0.7 + (1-beta) * twist_data.angular.z
+        # 좌회전인 경우는 반대. 
+        elif control_out[0] < control_out[2]:# and new_ang < 0:
+            twist_data.angular.z = -(0.01 + collision_prob[0] + control_out[2]) * beta * 0.7 + (1-beta) * twist_data.angular.z
+
+    # 충돌 확률이 높아 후진해야할 때, 후진 속도 제한
+    if twist_data.linear.x < -0.1:
+        twist_data.linear.x = -0.05
+
+    pub.publish(twist_data)
+
+    # 충돌 확률 : Matlab simulation을 위해 publish함.
+    cols = Float32()
+    cols.data = collision_prob[0]
+    pub2.publish(cols)
+    
+
+
 
 def main():
     # global twist_data
     image_topic = "/camera/depth/image_raw"
     odom_topic = "/odom"
 
-    # if has_reached:
-    #     return
-    # status_topic = "/gazebo/model_states/"
-    # rospy.Subscriber(status_topic, ModelStates, status_callback)
     rospy.Subscriber(image_topic, Image, image_callback)
     rospy.Subscriber(odom_topic, Odometry, odom_callback)
     rospy.spin()
     
-
 if __name__ == '__main__':
     # has_reached = False
     control_out = 0
